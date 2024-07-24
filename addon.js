1cd603327305ce984ba7ebeb6c263d37d6a8a445
phantazma <nabil.amenchar1@gmail.com>
2024-07-25 01:19:41 +0200

    Update addon.js

--- addon.js
+++ addon.js
@@ -7,7 +7,7 @@ const { mainFunction,checkProviders } = require('./server');
 var providers = []
 var allProviders = [ 'cineZone', 'pusatFilm', 'upMovies' ]
 
-app.use(express.static(path.join(__dirname)));
+addon.use(express.static(path.join(__dirname)));
 
 var MANIFEST = {
     id: 'amenco.phantom',
